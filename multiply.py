bc2d6299afec26481a902d7312ff7cbdafc1f172
hannahne <51098196+hannahne@users.noreply.github.com>
2023-02-13 21:25:49 +0100

    tidied up

--- multiply.py
+++ multiply.py
@@ -1,12 +1,14 @@
 from pypdf import PdfWriter
 import sys
+
 c = int(sys.argv[2])
+
 merger = PdfWriter()
 pdfObj = open(sys.argv[1], 'rb')
-x = 0
+
 for x in range(0, c):
     merger.append(pdfObj)
-    x =+1
+
 output = open(sys.argv[3], 'wb')
 merger.write(output)
 output.close()
