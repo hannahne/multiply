0d3945f2ce655c03c6e7239484b8cde576d13d1f
hannahne <51098196+hannahne@users.noreply.github.com>
2023-02-13 21:08:46 +0100

    added parameters

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
 from pypdf import PdfWriter
 import sys
+c = int(sys.argv[2])
 merger = PdfWriter()
-pdfObj = open('test.pdf', 'rb')
+pdfObj = open(sys.argv[1], 'rb')
 x = 0
-for x in range(0, 30):
+for x in range(0, c):
     merger.append(pdfObj)
     x =+1
-#output = open('output.pdf', 'wb')
-#merger.write(output)
-merger.write(sys.stdout)
-merger.close()
-#output.close()
+output = open(sys.argv[3], 'wb')
+merger.write(output)
+output.close()
