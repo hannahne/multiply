9f092e707fee91442b37774f052d92d6ff954a5c
hannahne <51098196+hannahne@users.noreply.github.com>
2023-02-13 20:43:37 +0100

    pdf multiply function

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("hello world")
+from pypdf import PdfWriter
+import sys
+merger = PdfWriter()
+pdfObj = open('test.pdf', 'rb')
+x = 0
+for x in range(0, 30):
+    merger.append(pdfObj)
+    x =+1
+#output = open('output.pdf', 'wb')
+#merger.write(output)
+merger.write(sys.stdout)
+merger.close()
+#output.close()
